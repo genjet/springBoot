df082939ede26077e90dfa385083503947625670
genjet <gengywang@gmail.com>
2024-10-18 18:01:51 +0800

    demo6

---
+++ lab6/demo6/src/test/java/com/uuu/demo6/Demo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuu.demo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
